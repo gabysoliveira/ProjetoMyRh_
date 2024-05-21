fe423d42a63eab39a5bd945f269ef78319e63405
Gabriele Silva Oliveira <g.b.silva.oliveira@avanade.com>
2024-05-21 14:07:59 -0300

    client project added

---
+++ ProjetoMyRh.ClienteAPI/Services/CandidatosService.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http.Headers;
+
+namespace ProjetoMyRh.ClienteAPI.Services
+{    public class CandidatosService
+    {
+        private readonly HttpClient httpClient;
+
+        public CandidatosService(IHttpClientFactory client)
+        {
+            httpClient = client.CreateClient();
+
+            httpClient.BaseAddress = new Uri("http://localhost:5179/");
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+
+
+    }
+}
